2c2238331728846d64cf06b95d0a18ef3e213b56
kjusino <kennethjusino@hotmail.com>
2024-06-23 16:03:19 -0400

    added netflix

--- src/articles/allData.tsx
+++ src/articles/allData.tsx
@@ -1,7 +1,8 @@
 import Home from './Home';
 import About from './About';
 import Pa from './Pa';
+import Netflix from './Netflix';
 
-const allData = [Home, About, Pa];
+const allData = [Home, About, Pa, Netflix];
 
 export default allData;
